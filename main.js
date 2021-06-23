c1ec6525dde30e7fd892f294fc8ef1984a5a6d55
innerfire2 <73815918+innerfire2@users.noreply.github.com>
2021-06-23 15:45:00 +0200

    finished assets and addet evolution

--- main.js
+++ main.js
@@ -13,6 +13,9 @@ class Screen {
         this.timeGame = loadImage("assets/timeGame.png");
         this.winScreen = loadImage("assets/winScreen.png");
         this.loseScreen = loadImage("assets/loseScreen.png");
+        this.evolvingMessage = loadImage("assets/evolvingMessage.png");
+        this.umbreon = loadImage("assets/umbreon.png");
+        this.espeon = loadImage("assets/espeon.png");
 
         //objects
         this.gameOne = new TimeGame();
@@ -23,9 +26,13 @@ class Screen {
         this.timeGameButton = new Button(30, 258, 108, 98);
         this.continueButton = new Button(130, 325, 159, 35);
         this.startButton = new Button (160, 330, 125, 38);
+        this.evolvingButton = new Button (137, 253, 159, 35);
+        this.restartButton = new Button (147, 342, 122, 28);
     }
 
     display(){
+
+        //start
         if (this.state === "start"){
             image (this.startScreen, 0, 0, 400, 400);
             
@@ -33,7 +40,8 @@ class Screen {
                 this.state = "main";
             }
         }
-            
+        
+        //main
         if (this.state === "main"){
             image (this.mainScreen, 0, 0, 400, 400);
             this.friendship.display(); 
@@ -45,12 +53,13 @@ class Screen {
             this.gameOne.resetWhenWinning();   
         } 
 
+        //timeGame
         if (this.state === "timeGame"){
             image (this.timeGame, 0, 0, 400, 400);
             this.gameOne.display();
             this.gameOne.resetWhenLosing();
             
-            // changing screen according to result from timeGame
+            //changing state and amount of hearts
             if (this.gameOne.winning()){
             this.state = "winScreen";
             this.friendship.addingHearts();
@@ -61,6 +70,7 @@ class Screen {
             }
         }
 
+        //winScreen and loseScreen
         if (this.state === "winScreen"){
             image (this.winScreen, 0, 0, 400, 400);
 
@@ -77,16 +87,37 @@ class Screen {
             }
         }
 
-        if (this.friendship.heartsFull()){
+        //evolving
+        if (this.friendship.heartsFull() && this.state === "main" ){
 
             this.state = "evolving";
+             
 
-            if(this.dayOrNight.dayTime()){
-                console.log("day");
-            } else {
-                console.log("night");
-            }
         }
+
+        if (this.state === "evolving"){
+            
+            image (this.evolvingMessage, 0, 0, 400, 400);
+        }
+
+        if (this.evolvingButton.hitTest() && mouseIsPressed){
+                this.state = "evolved";
+            }
+        
+        if (this.state === "evolved"){
+
+           if(this.dayOrNight.dayTime()){
+                image (this.espeon, 0, 0, 400, 400);
+            } else {
+                image (this.umbreon, 0, 0, 400, 400);
+            } 
+
+            if (this.restartButton.hitTest() && mouseIsPressed && this.state === "evolved"){
+
+                this.state = "start";
+                this.friendship.hearts = 0;
+            }
+        }  
     }
 }
 
@@ -96,6 +127,8 @@ let background = new Screen();
 function draw () {
     clear();
     background.display();
+
+    console.log(background.state);
 
 }
 
